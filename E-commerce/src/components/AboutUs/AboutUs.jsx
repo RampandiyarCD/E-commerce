9906cc0e163c5bc190caf13b2efb6e126d718a78
sulthan1866cd <sulthan.kasim@crystaldelta.com>
2025-07-31 16:04:09 +0530

    ▼ added to about us

--- E-commerce/src/components/AboutUs/AboutUs.jsx
+++ E-commerce/src/components/AboutUs/AboutUs.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React from "react";
 import AboutUsCard from "./AboutUsCard";
 import QNA from "../../utils/aboutUs";
 
--- E-commerce/src/components/AboutUs/AboutUsCard.jsx
+++ E-commerce/src/components/AboutUs/AboutUsCard.jsx
@@ -1,13 +1,19 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import "./aboutUs.css";
 const AboutUsCard = ({ props }) => {
   const { q, a } = props;
   const tabRef = useRef();
-
+  const [isOpen, setOpen] = useState(false);
+  const toggleTab = () => {
+    setOpen(!isOpen);
+  };
   return (
     <div className="aboutUs-tab">
       <details>
-        <summary>{q}</summary>
+        <summary onClick={toggleTab}>
+          <p>{q}</p>
+          <p> {isOpen ? "▼" : "▲"}</p>
+        </summary>
         <div className="aboutUs-tab-open" ref={tabRef}>
           <span>{a}</span>
         </div>
