be6314949238a1fb244ae6432bf0ed50573f10d6
sangeetha <sangeetha.sakthimurugan@crystaldelta.com>
2025-07-31 23:56:03 +0530

    product-list is completed.

---
+++ E-commerce/src/components/Datas.js
@@ -0,0 +1,39 @@
+// data.js
+const products = [
+  {
+    id: 1,
+    name: "Product A",
+    image: "/image1.jpg",
+    price: "$10",
+    description: "This is the product 1",
+  },
+  {
+    id: 2,
+    name: "Product B",
+    image: "/img2.jpg",
+    price: "$20",
+    description: "this is the product 2",
+  },
+  {
+    id: 3,
+    name: "Product C",
+    image: "/img3.jpg",
+    price: "$30",
+    description: "this is the product 3",
+  },
+  {
+    id: 4,
+    name: "Product D",
+    image: "/img4.jpg",
+    price: "$40",
+    description: "this is the product 4",
+  },
+  {
+    id: 5,
+    name: "Product E",
+    image: "/img5.jpg",
+    price: "$50",
+    description: "This is the product 5",
+  },
+];
+export default products;
