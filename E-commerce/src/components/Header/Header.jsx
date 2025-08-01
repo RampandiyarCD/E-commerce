e55bca7dbe212e3929483f8023032b6d8272acac
sulthan1866cd <sulthan.kasim@crystaldelta.com>
2025-08-01 12:25:44 +0530

    added nav contents and view cart to header

--- E-commerce/src/components/Header/Header.jsx
+++ E-commerce/src/components/Header/Header.jsx
@@ -1,7 +1,22 @@
 import React from "react";
-import "./header.css"
-const Header = () => {
-  return <div className="header">Header</div>;
+import "./header.css";
+import ViewCart from "../AddToCart/ViewCart";
+const Header = ({ props }) => {
+  return (
+    <>
+      <div className="header">
+        Header Store
+        <span>
+          <span>products</span>
+          <span>FAQs</span>
+          <span>About the store</span>
+          <span>
+            <ViewCart props={props} />
+          </span>
+        </span>
+      </div>
+    </>
+  );
 };
 
 export default Header;
