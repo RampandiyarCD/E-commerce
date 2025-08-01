e6143dedede45866f5b6d509832ffcf958da340c
sangeetha <sangeetha.sakthimurugan@crystaldelta.com>
2025-08-01 16:04:28 +0530

    changes made

--- E-commerce/src/App.jsx
+++ E-commerce/src/App.jsx
@@ -1,11 +1,18 @@
 import products from "./components/Datas"
-import ProductList from "./components/ProductList"
+import ProductList from "./components/ProductList/ProductList"
+import { NotifiyPopup,showMessage } from "./components/Notifications/NotifyPopup"
+import Navigation from "./components/Navigation/Navigation"
+import Footer from "./components/Footer/Footer"
 
 
 export default function App() {
   return (
     <div>
+      <Navigation />     
+      <NotifiyPopup />
+      <button onClick={()=>{showMessage("The user is created.","created")}}>Click me</button>
       <ProductList product={products} />
+      <Footer />
      </div>
   )
 }
